1e4ce005a08a54ab113eeeaccc3a7dc330afff90
Azimjon Alijonov <2110040@newuu.local>
2023-05-11 23:04:51 +0500

    wewewew

---
+++ src/Payment.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Payment {
+    private  int bill=0;
+    Ordering ordering =new Ordering();
+    List<FoodsAndDrinks> orders =ordering.select();
+
+    public int calculate(){
+        for (FoodsAndDrinks food:orders
+             ) {
+
+            bill=bill+food.getPrice();
+        }
+        return bill;
+    }
+}
